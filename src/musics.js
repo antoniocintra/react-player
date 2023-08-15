ace156e1b0e427da80dc99bd7ebb519cb6eb41b2
antoniocintra <aecf12@hotmail.com>
2023-08-15 15:32:36 -0300

    first commit

---
+++ src/musics.js
@@ -0,0 +1,30 @@
+export const musics = [
+    {
+
+        id: 1,
+        title: "Violão Acústico",
+        artist: 'Mayer Bronkx',
+        description: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make",
+        url: 'https://www.4shared.com/mp3/vwq1jROTku/cleaninoutmycloset.html]cleanin&#39;out.my.closet.mp3',
+        cover: 'https://storage.googleapis.com/pedagogico/frontend-files/aula-react-referencias-eventos/img1.jpg'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+]
